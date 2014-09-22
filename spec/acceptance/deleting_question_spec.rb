5880a92c169728bbe9259c86899f8233e98a74ac
Быкова Виктория Олеговна <vbykova@at-consulting.ru>
2014-09-22 17:09:49 +0400

    added acceptance test

---
+++ spec/acceptance/deleting_question_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'delete question', %q{
+	As an authorized user I want to
+	delete question that was asked by me
+} do
+
+	given(:user) { FactoryGirl.create(:user) }
+	given(:another_user) { FactoryGirl.create(:user) }
+	given(:his_question) { FactoryGirl.create(:question, user: user) }
+	given(:another_question) { FactoryGirl.create(:question, user: another_user) }
+
+
+	before { login_as user }
+
+	scenario 'can delete his question' do
+		visit question_path(his_question)
+		expect(page).to have_link 'delete'
+	end
+
+	scenario 'cannot delete another question' do
+		visit question_path(another_question)
+		expect(page).not_to have_link 'delete'
+	end
+end
