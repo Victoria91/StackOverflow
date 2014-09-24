95bcb7cd632bf99c74bc503130b586d59ad5c344
Быкова Виктория Олеговна <vbykova@at-consulting.ru>
2014-09-24 13:10:12 +0400

    refactored some specs

--- spec/acceptance/viewing_answers_spec.rb
+++ spec/acceptance/viewing_answers_spec.rb
@@ -6,15 +6,12 @@ feature 'viewing answers', %q{
 	know the solution
 } do
 	
-	given(:question) { FactoryGirl.create(:question) }
-	given(:answer) { FactoryGirl.create(:answer, question: question) }
+	given!(:question) { FactoryGirl.create(:question) }
+	given!(:answer) { FactoryGirl.create(:answer, question: question) }
 
 	scenario 'view answer to a given question' do
-		question
-		answer
 		visit question_path(question)
 		expect(page).to have_content question.body
-		#save_and_open_page
 		expect(page).to have_content answer.body
 	end
 
--- spec/acceptance/viewing_questions_spec.rb
+++ spec/acceptance/viewing_questions_spec.rb
@@ -6,12 +6,10 @@ feature 'view questions', %q{
 	to find the similar problem that I have
 } do
 	
-	given(:questions) { FactoryGirl.create_list(:question, 5) }
+	given!(:questions) { FactoryGirl.create_list(:question, 5) }
 
 	scenario 'view questions' do
-		questions
 		visit questions_path
-		#save_and_open_page
 		questions.each do |q|
 			expect(page).to have_link q.title
 		end
