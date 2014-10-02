a910f952f818afdc69b4344e8468b1800113e5fe
Быкова Виктория Олеговна <vbykova@at-consulting.ru>
2014-10-02 12:02:38 +0400

    updated edit answer spec so that it contains cancel editing

--- spec/acceptance/editing_answer_spec.rb
+++ spec/acceptance/editing_answer_spec.rb
@@ -12,46 +12,63 @@ feature 'editing_answer', %q{
 	given!(:answer) { FactoryGirl.create(:answer, user: user, question: question) }
 	given!(:another_answer) { FactoryGirl.create(:answer, user: another_user, question: question) }
 
+	context 'authorized' do
+		before { login_as user }
 
-	scenario 'editing own answer with valid attributes', js: true do
-		login_as user
-		visit question_path(question)
-		find('.editable_answer', :text => answer.body).click 
-		within '.editable_answer_form' do
-			expect(page).to have_selector 'textarea'
-			fill_in 'answer[body]', with: 'new answer'
-			click_on 'Update Answer'
-		end
-		within '.answers' do
-			expect(page).to have_content 'new answer'
-			expect(page).not_to have_selector 'textarea'
-			expect(page).not_to have_content answer.body
-		end
-	end
+		context 'edit own answer' do
+			before do 
+				visit question_path(question)
+				find('.editable_answer', :text => answer.body).click 
+			end
 
-	scenario 'editing own answer with invalid attributes', js: true do
-		login_as user
-		visit question_path(question)
-		find('.editable_answer', :text => answer.body).click 
-		within '.editable_answer_form' do
-			expect(page).to have_selector 'textarea'
-			fill_in 'answer[body]', with: ''
-			click_on 'Update Answer'
-		end
-		within '.answers' do
-			expect(page).to have_selector 'textarea'
-			expect(page).to have_selector '.answer_errors'
-		end
-	end
+			scenario 'editing own answer with valid attributes', js: true do
+				within '.editable_answer_form' do
+					expect(page).to have_selector 'textarea'
+					fill_in 'answer[body]', with: 'new answer'
+					click_on 'Update Answer'
+				end
+				within '.answers' do
+					expect(page).to have_content 'new answer'
+					expect(page).not_to have_selector 'textarea'
+					expect(page).not_to have_content answer.body
+				end
+			end
 
-	scenario 'editing another answer' do
-		login_as user
-		visit question_path(question)
-		expect(page).to have_content(answer.body)
-		expect(page).to have_content(another_answer.body)
-		within '.editable_answer' do
-			expect(page).not_to have_content another_answer.body
+			scenario 'editing own answer with invalid attributes', js: true do
+				within '.editable_answer_form' do
+					expect(page).to have_selector 'textarea'
+					fill_in 'answer[body]', with: ''
+					click_on 'Update Answer'
+				end
+				within '.answers' do
+					expect(page).to have_selector 'textarea'
+					expect(page).to have_selector '.answer_errors'
+				end
+			end
+
+			scenario 'cancel edit', js: true do
+				within '.editable_answer_form' do
+					fill_in 'answer[body]', with: 'some strange text'
+					click_button 'Cancel'
+					expect(page).not_to have_selector 'textarea'
+				end
+				within '.answers' do
+					expect(page).to have_content answer.body
+					expect(page).not_to have_content 'some strange text'
+				end
+			end
+
 		end
+
+		scenario 'editing another answer' do
+			visit question_path(question)
+			expect(page).to have_content(answer.body)
+			expect(page).to have_content(another_answer.body)
+			within '.editable_answer' do
+				expect(page).not_to have_content another_answer.body
+			end
+		end
+
 	end
 
 	scenario 'unauthorized user cannot edit answers' do
