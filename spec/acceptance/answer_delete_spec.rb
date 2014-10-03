f8e8601b9ad9bf58bb14de065905f9b7d4c4ccca
Быкова Виктория Олеговна <vbykova@at-consulting.ru>
2014-10-03 16:10:12 +0400

    acceptance spec is done

---
+++ spec/acceptance/answer_delete_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+feature 'delete answer', %q{
+	As an owner of an answer
+	I want to delete question
+} do
+	
+	given(:user) { FactoryGirl.create(:user) }
+	given(:another_user) { FactoryGirl.create(:user) }
+	given(:question) { FactoryGirl.create(:question, user: user) }
+	given!(:answer) { FactoryGirl.create(:answer, question: question, user: user) }
+	given!(:another_answer) { FactoryGirl.create(:answer, question: question, user: another_user) }
+
+	context 'authorized' do
+		before do 
+			login_as user
+			visit question_path(question)
+		end
+
+		scenario 'user can delete his answer' do
+			within '.answers' do
+				find("#delete_answer_#{answer.id}_link").click
+				expect(page).not_to have_content answer.body
+			end
+		end
+
+		scenario 'user cannot delete another answer' do
+			within '.answers' do
+				expect(page).not_to have_selector "#delete_answer_#{another_answer.id}_link"
+			end
+		end
+	end
+
+	scenario 'unauthorized' do
+		visit question_path(question)
+		within '.answers' do
+			expect(page).not_to have_selector "#delete_answer_#{another_answer.id}_link"
+		end
+	end
+	
+end
