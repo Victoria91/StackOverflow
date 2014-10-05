ee5764e4253365e13c99cf67ae2288ffac64926b
Victoria91 <vi4ka_91@mail.ru>
2014-10-05 17:17:28 +0300

    model spec now pass

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,15 @@ class Question < ActiveRecord::Base
 
 	validates :title, :body, presence: true
 	validates :title, length: { maximum: 255 }
+
+
+  def accepted_answer 
+    self.answers.find_by(accepted: true)
+  end
+
+  def toggle_accepted(answer) 
+    accepted_answer.update!(accepted: false) if accepted_answer and !answer.accepted?
+		answer.toggle(:accepted).save!
+	end
+
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -7,19 +7,22 @@ RSpec.describe Question, :type => :model do
   it { should have_many :answers }
   it { should ensure_length_of(:title).is_at_most(255) }
 
-  let(:question) { FactoryGirl.create(:question) }
-  let(:answer_one) { FactoryGirl.create(:answer, question: question) }
-  let(:answer_two) { FactoryGirl.create(:answer, question: question) }
+  let!(:question) { FactoryGirl.create(:question) }
+  let!(:answer_one) { FactoryGirl.create(:answer, question: question) }
+  let!(:answer_two) { FactoryGirl.create(:answer, question: question) }
 
-  it '#accept changes accepted state' do
-  	expect{ question.toggle_accept(answer_one) }.to change(answer_one, :accepted).to true
+  it '#toggle_accepted changes accepted state to true for unaccepted' do
+  	expect{ question.toggle_accepted(answer_one) }.to change(answer_one, :accepted).to true
   end
 
   it 'only one answer can be accepted' do
-  	question.toggle_accept(answer_two)
-  	expect{ question.toggle_accept(answer_one) }.to change(question.answer_two, :accepted).to false
+    question.toggle_accepted(answer_two)
+  	question.toggle_accepted(answer_one) 
+    expect(question.answers.where(accepted: true).count).to eq(1)
   end
 
-  it 'reaccepts changes accept state' do
+  it '#toggle_accepted changes accept accepted state to false for accepted' do
+    answer_one.update!(accepted: true)
+    expect{ question.toggle_accepted(answer_one) }.to change(answer_one.reload, :accepted).to false
   end
 end
