f4cdff77e4ca40df2633f88cdcd7dd49d444129d
Victoria91 <vi4ka_91@mail.ru>
2014-11-12 21:50:09 +0200

    users api in progress

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -3,5 +3,10 @@ class Api::V1::ProfilesController < Api::V1::BaseController
   def me
     respond_with current_resource_owner
   end
+
+  def users
+    @users = User.all
+    respond_with @users
+  end
   
 end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -38,4 +38,50 @@ describe 'Profile API' do
       end
     end
   end
+
+  describe 'GET/users' do
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get '/api/v1/profiles/users', format: :json
+        expect(response.status).to eq(401)
+      end
+
+      it 'returns 401 status if access_token is invalid' do
+        get '/api/v1/profiles/users', format: :json, access_token: '12412412424'
+        expect(response.status).to eq(401)
+      end
+
+    end
+
+    context 'authorized' do
+      let!(:users) { create_list(:user, 5) }
+      let(:access_token) { create(:access_token, resource_owner_id: users.last.id ) }
+
+      before { get '/api/v1/profiles/users', format: :json, access_token: access_token.token }
+
+      it 'returns 200 status' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns list of users' do 
+        expect(response.body).to have_json_size(users.length).at_path("profiles")
+      end
+
+      %w(email id created_at updated_at).each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to be_json_eql(users.first.send(attr.to_sym).to_json).at_path("profiles/0/#{attr}")
+        end
+      end
+
+      %w(password encrypted_password).each do |attr| 
+        it "does not contain #{attr}" do
+          expect(response.body).to_not have_json_path("profiles/0/#{attr}")
+        end
+      end
+
+      it 'does not return an authorized user' do 
+        expect(assigns(:users)).not_to include(users.last)
+      end
+    end
+  end
 end
