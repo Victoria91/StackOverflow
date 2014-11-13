c6da30b69ad28a650ff6de6fc08d33e9fb594d87
Быкова Виктория Олеговна <vbykova@at-consulting.ru>
2014-11-13 18:27:55 +0300

    question api in progress

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,5 +1,5 @@
 class QuestionSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :created_at, :updated_at
 
-  has_many :answers
+  has_many :answers, :attachments
 end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper' 
+require 'rails_helper'
 
 describe 'Questions API' do
   describe 'GET/index' do
@@ -12,7 +12,6 @@ describe 'Questions API' do
         get '/api/v1/questions', format: :json, access_token: '123456'
         expect(response.status).to eq(401)
       end
-
     end
 
     context 'authorized' do
@@ -27,7 +26,7 @@ describe 'Questions API' do
       end
 
       it 'returns list of questions' do
-        expect(response.body).to have_json_size(5).at_path("questions")
+        expect(response.body).to have_json_size(5).at_path('questions')
       end
 
       %w(id title body created_at updated_at).each do |attr|
@@ -37,7 +36,7 @@ describe 'Questions API' do
       end
 
       it 'returns answers' do
-        expect(response.body).to have_json_size(1).at_path("questions/0/answers")
+        expect(response.body).to have_json_size(1).at_path('questions/0/answers')
       end
 
       %w(id body created_at updated_at).each do |attr|
@@ -45,8 +44,45 @@ describe 'Questions API' do
           expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("questions/0/answers/0/#{attr}")
         end
       end
+    end
+  end
 
+  describe 'GET/questions/id' do
+    let(:question) { create(:question) }
+    let!(:attachment) { create(:attachment, attachmentable_type: 'Question', attachmentable_id: question.id ) }
+    
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get "/api/v1/questions/#{question.id}", format: :json
+        expect(response.status).to eq(401)
+      end
 
+      it 'returns 401 status if access_token is invalid' do
+        get "/api/v1/questions/#{question.id}", format: :json, access_token: '123456'
+        expect(response.status).to eq(401)
+      end
+
+    end
+
+    context 'authorized' do
+      let(:access_token) { create(:access_token) }
+      before { get "/api/v1/questions/#{question.id}", format: :json, access_token: access_token.token }
+
+      it 'returns 200 status' do
+        expect(response.status).to eq(200)
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("question/#{attr}")
+        end
+      end
+
+      context 'attachment' do
+        it 'returns attachment' do
+          expect(response.body).to have_json_size(1).at_path('question/attachments')
+        end
+      end
     end
   end
 end
