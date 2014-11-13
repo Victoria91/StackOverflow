6c1f2c712a5d8c312e8f03deb462a5644b46d07d
Victoria91 <vi4ka_91@mail.ru>
2014-11-13 21:40:26 +0200

    question api done

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -82,6 +82,16 @@ describe 'Questions API' do
         it 'returns attachment' do
           expect(response.body).to have_json_size(1).at_path('question/attachments')
         end
+
+        %w(id created_at updated_at).each do |attr|
+          it "contains #{attr}" do
+            expect(response.body).to be_json_eql(attachment.send(attr.to_sym).to_json).at_path("question/attachments/0/#{attr}")
+          end
+        end
+
+        it 'context file url' do
+          expect(response.body).to be_json_eql(attachment.file.url.to_json).at_path("question/attachments/0/file/url")
+        end
       end
     end
   end
