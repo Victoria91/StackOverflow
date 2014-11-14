ce7dd2e29c278ccec9b2730b02aa2e651b643619
Быкова Виктория Олеговна <vbykova@at-consulting.ru>
2014-11-14 14:08:28 +0300

    answers api done

--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe 'Answers API' do
   describe 'GET/index' do
     let(:question) { create(:question) }
+    let!(:answers) { create_list(:answer, 5, question: question) }
+
     context 'unauthorized' do
       it 'returns 401 status if there is no access_token' do
         get "/api/v1/questions/#{question.id}/answers", format: :json
@@ -21,6 +23,16 @@ describe 'Answers API' do
 
       it 'returns 200 status' do
         expect(response.status).to eq(200)
+      end
+
+      it 'returns list of answers' do
+        expect(response.body).to have_json_size(5).at_path('answers')
+      end
+
+      %w(id body question_id created_at updated_at user_id accepted).each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to be_json_eql(answers.first.send(attr.to_sym).to_json).at_path("answers/0/#{attr}")
+        end
       end
     end
 
