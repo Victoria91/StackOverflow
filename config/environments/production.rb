62e3be575ccdafc2b1875dd7828195032036d700
Быкова Виктория Олеговна <vbykova@at-consulting.ru>
2014-12-11 17:18:12 +0300

    updated mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,13 @@ Rails.application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
+  config.action_mailer.default_url_options = { host: '128.199.63.177' }
+# ActionMailer Config
+# Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
