11dec40727333480cc61d0505906fec44f38a3c1
Быкова Виктория Олеговна <vbykova@at-consulting.ru>
2014-12-18 18:43:50 +0300

    controller test done

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -263,5 +263,22 @@ RSpec.describe QuestionsController do
     end
 
   end
+
+  describe '#digest_unsubscribe' do
+    context 'authorized' do
+      sign_in_user
+
+      it 'changes digest state' do
+        expect { get :digest_unsubscribe }.to change { user.reload.digest }.to false
+      end 
+    end
+
+    context 'unauthorized' do
+      it 'redirects' do
+        get :digest_unsubscribe
+        expect(response).to be_redirect
+      end
+    end
+  end
  
 end
