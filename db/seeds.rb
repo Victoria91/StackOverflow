5dfd2851fc784d4766e4a3ba5d1dd09384b032fd
Быкова Виктория Олеговна <vbykova@at-consulting.ru>
2014-12-25 14:47:10 +0300

    added tags in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts 'Creating tags...'
+tags = Tag.first_or_create([
+  { name: 'Ruby' },
+  { name: 'Rails' },
+  { name: 'JQuery' },
+  { name: 'Javascript' },
+  { name: 'Python' },
+  { name: 'SQL' },
+  { name: 'Unix' },
+  { name: 'Windows' },
+  { name: 'C++' },
+  { name: 'VIM' },
+  { name: 'Other' }
+])
