d563d066dc3b438581748cc3290eb2d2aab9a04d
Быкова Виктория Олеговна <vbykova@at-consulting.ru>
2014-12-25 12:43:07 +0300

    added uniqueness validation for tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,6 @@ class Tag < ActiveRecord::Base
   has_many :question_tags, dependent: :destroy
   has_many :questions, through: :question_tags
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :name, length: { maximum: 255 }
-
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe Tag do
   it { should have_many(:question_tags).dependent(:destroy) }
   it { should validate_presence_of(:name) }
   it { should ensure_length_of(:name).is_at_most(255) }
+  it { should validate_uniqueness_of(:name) }
 end
