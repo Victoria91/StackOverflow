e25e8d23b7dff94b9d8159bef528309a5626f003
Быкова Виктория Олеговна <vbykova@at-consulting.ru>
2015-01-12 17:15:30 +0300

    acceptance spec done

---
+++ spec/acceptance/answers/vote_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+feature 'vote for an answer', %q(
+  As a "User"
+  I want to vote for an answer
+  In order to mark it's significance
+) do
+  
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given!(:answer) { create(:answer, question: question) }
+  given!(:another_answer) { create(:answer, question: question) }
+  given!(:own_answer) { create(:answer, question: question, user: user) }
+
+  context 'authorized' do
+    scenario 'cannot vote' do
+      visit question_path(question)
+      within "##{answer.id}" do
+        expect(page).not_to have_selector ".vote_up_link"
+        expect(page).not_to have_selector ".vote_down_link"
+      end
+    end
+  end
+
+  context 'anauthorized' do
+    background do
+      login_as user
+      visit question_path(question)
+    end
+
+    scenario 'cannot vote for own question' do
+      within "##{own_answer.id}" do
+        expect(page).not_to have_selector ".vote_up_link"
+        expect(page).not_to have_selector ".vote_down_link"
+      end
+    end
+
+    scenario 'vote up for another question' do
+      within "##{another_answer.id}" do
+        expect(page).to have_content answer.rating
+        find('.vote_up_link').click
+        expect(page).to have_content answer.rating + 1
+      end
+    end
+
+    scenario 'vote down for another question' do
+      within "##{another_answer.id}" do
+        expect(page).to have_content answer.rating
+        find('.vote_down_link').click
+        expect(page).to have_content answer.rating - 1
+      end
+    end
+
+    scenario 'cannot vote up twice' do
+      within "##{another_answer.id}" do
+        find('.vote_up_link').click
+        expect(page).not_to have_selector '.vote_up_link'
+      end
+    end
+    
+    scenario 'cannot vote down twice' do
+      within "##{another_answer.id}" do
+        find('.vote_down_link').click
+        expect(page).not_to have_selector '.vote_down_link'
+      end
+    end
+
+  end
+
+end
